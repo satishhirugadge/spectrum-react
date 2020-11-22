241f3e2546884955406e20b9417e653a7f4ef9c1
Satish Gadge <satishgadge14@gmail.com>
2020-11-22 13:49:21 -0500

    Initial commit project setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Restro from './views/Restro.js';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <Restro />
+    </div>
+  );
+}
+
+export default App;
