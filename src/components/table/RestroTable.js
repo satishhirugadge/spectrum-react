3b2ed8fddd0f5927c964b921d0c30ed4705de6e8
Satish Gadge <satishgadge14@gmail.com>
2020-11-23 06:42:12 -0500

    Update Table component to add react bootstrap

---
+++ src/components/table/RestroTable.js
@@ -0,0 +1,35 @@
+import Table from 'react-bootstrap/Table';
+import Row from 'react-bootstrap/Row';
+
+function RestroTable(props) {
+  return (
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          {props.cols.map((col) => (
+            <th key={col.name}> {col.header}</th>
+          ))}
+        </tr>
+      </thead>
+      {props.rows.length !== 0 && (
+        <tbody>
+          {props.rows.map((row) => (
+            <tr key={row.id}>
+              {props.cols.map((col) => (
+                <td key={col.name}>
+                  {col.name === 'genre'
+                    ? row.genreArray.map((v) => {
+                        return <div key={v}>{v}</div>;
+                      })
+                    : row[col.name]}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      )}
+    </Table>
+  );
+}
+
+export default RestroTable;
